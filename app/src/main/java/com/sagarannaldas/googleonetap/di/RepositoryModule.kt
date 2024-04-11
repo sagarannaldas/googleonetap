4906c4fbae9416e18db7d446485984cff87fe45c
sagarannaldas <annal7057@gmail.com>
2024-04-11 17:04:14 +0530

    1. Created RepositoryModule

---
+++ app/src/main/java/com/sagarannaldas/googleonetap/di/RepositoryModule.kt
@@ -0,0 +1,42 @@
+package com.sagarannaldas.googleonetap.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.sagarannaldas.googleonetap.data.repository.DataStoreOperationsImpl
+import com.sagarannaldas.googleonetap.data.repository.RepositoryImpl
+import com.sagarannaldas.googleonetap.domain.repository.DataStoreOperations
+import com.sagarannaldas.googleonetap.domain.repository.Repository
+import com.sagarannaldas.googleonetap.util.Constants.PREFERENCES_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideDataStorePreferences(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile(PREFERENCES_NAME) }
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataStoreOperations(dataStore: DataStore<Preferences>): DataStoreOperations {
+        return DataStoreOperationsImpl(dataStore = dataStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(dataStoreOperations: DataStoreOperations): Repository {
+        return RepositoryImpl(dataStoreOperations = dataStoreOperations)
+    }
+}
