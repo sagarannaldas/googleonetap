bb80631390f90e2d18cc0f8fa620a0fe618497f3
sagarannaldas <annal7057@gmail.com>
2024-04-11 17:28:43 +0530

    1. Created LoginViewModel

--- app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/login/LoginScreen.kt
@@ -3,19 +3,29 @@ package com.sagarannaldas.googleonetap.presentation.screen.login
 import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.sagarannaldas.googleonetap.domain.model.MessageBarState
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun LoginScreen(navController: NavHostController) {
+fun LoginScreen(
+    navController: NavHostController,
+    loginViewModel: LoginViewModel = hiltViewModel()
+) {
+    val signedInState by loginViewModel.signedInState
+    val messageBarState by loginViewModel.messageBarState
+
     Scaffold(
         topBar = { LoginTopBar() },
         content = {
             LoginContent(
-                signedInState = true,
-                messageBarState = MessageBarState(),
-                onButtonClick = {}
+                signedInState = signedInState,
+                messageBarState = messageBarState,
+                onButtonClick = {
+                    loginViewModel.saveSignedInState(signedIn = true)
+                }
             )
         }
     )
---
+++ app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/login/LoginViewModel.kt
@@ -0,0 +1,38 @@
+package com.sagarannaldas.googleonetap.presentation.screen.login
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sagarannaldas.googleonetap.domain.model.MessageBarState
+import com.sagarannaldas.googleonetap.domain.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+    private val _signedInState: MutableState<Boolean> = mutableStateOf(false)
+    val signedInState: State<Boolean> = _signedInState
+
+    private val _messageBarState: MutableState<MessageBarState> = mutableStateOf(MessageBarState())
+    val messageBarState: State<MessageBarState> = _messageBarState
+
+    init {
+        viewModelScope.launch {
+            repository.readSignedInState().collect { state ->
+                _signedInState.value = state
+            }
+        }
+    }
+
+    fun saveSignedInState(signedIn: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.saveSignedInState(signedIn = signedIn)
+        }
+    }
+}
