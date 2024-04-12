6db5bcf1310a704f3abfb3862daf50fa8924c786
sagarannaldas <annal7057@gmail.com>
2024-04-12 09:11:23 +0530

    1. Created One Tap Signin/ Signup method 2. Created StartActivityForResult method

--- app/src/main/java/com/sagarannaldas/googleonetap/util/Constants.kt
+++ app/src/main/java/com/sagarannaldas/googleonetap/util/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "http://10.0.2.2:8080"
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
     const val PREFERENCES_NAME = "app_preferences"
+    const val CLIENT_ID = "885871525573-bqd0qqmtb7bpn3kr8q4r88dp6ho1ufd4.apps.googleusercontent.com"
 }
