6bf7c4d4981482286b8cc7809fe7317261e6c17e
sagarannaldas <annal7057@gmail.com>
2024-04-19 19:40:36 +0530

    1. Created Profile Content

---
+++ app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/profile/ProfileContent.kt
@@ -0,0 +1,125 @@
+package com.sagarannaldas.googleonetap.presentation.screen.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import com.sagarannaldas.googleonetap.R
+import com.sagarannaldas.googleonetap.component.GoogleButton
+import com.sagarannaldas.googleonetap.component.MessageBar
+import com.sagarannaldas.googleonetap.domain.model.ApiResponse
+import com.sagarannaldas.googleonetap.domain.model.MessageBarState
+import com.sagarannaldas.googleonetap.ui.theme.LoadingBlue
+import com.sagarannaldas.googleonetap.util.RequestState
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun ProfileContent(
+    apiResponse: RequestState<ApiResponse>,
+    messageBarState: MessageBarState,
+    firstName: String,
+    lastName: String,
+    onFirsNameChanged: (String) -> Unit,
+    onLastNameChanged: (String) -> Unit,
+    emailAddress: String,
+    profilePhoto: String,
+    onSignOutClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            if (apiResponse is RequestState.Loading) {
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = LoadingBlue
+                )
+            } else {
+                MessageBar(messageBarState = messageBarState)
+            }
+        }
+        Column(
+            modifier = Modifier.weight(9f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CentralContent(
+                firstName = firstName,
+                onFirstNameChanged = onFirsNameChanged,
+                lastName = lastName,
+                onLastNameChanged = onLastNameChanged,
+                emailAddress = emailAddress,
+                profilePhoto = profilePhoto,
+                onSignOutClicked = onSignOutClicked
+            )
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+private fun CentralContent(
+    firstName: String,
+    onFirstNameChanged: (String) -> Unit,
+    lastName: String,
+    onLastNameChanged: (String) -> Unit,
+    emailAddress: String?,
+    profilePhoto: String?,
+    onSignOutClicked: () -> Unit
+) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(data = profilePhoto)
+            .crossfade(durationMillis = 1000)
+            .placeholder(drawableResId = R.drawable.ic_placeholder)
+            .transformations(CircleCropTransformation())
+            .build(),
+        contentDescription = "Profile Photo"
+    )
+    OutlinedTextField(
+        value = firstName,
+        onValueChange = { onFirstNameChanged(it) },
+        label = { Text(text = "First Name") },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        singleLine = true
+    )
+    OutlinedTextField(
+        value = lastName,
+        onValueChange = { onLastNameChanged(it) },
+        label = { Text(text = "Last Name") },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        singleLine = true
+    )
+    OutlinedTextField(
+        value = emailAddress.toString(),
+        onValueChange = { },
+        label = { Text(text = "Email Address") },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        singleLine = true,
+        enabled = false
+    )
+    GoogleButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp),
+        primaryText = "Sign Out",
+        secondaryText = "Sign Out",
+        onClick = onSignOutClicked
+    )
+}
--- app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/sagarannaldas/googleonetap/presentation/screen/profile/ProfileScreen.kt
@@ -1,9 +1,14 @@
 package com.sagarannaldas.googleonetap.presentation.screen.profile
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.sagarannaldas.googleonetap.domain.model.ApiResponse
+import com.sagarannaldas.googleonetap.domain.model.MessageBarState
+import com.sagarannaldas.googleonetap.util.RequestState
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProfileScreen(navController: NavHostController) {
     Scaffold(
@@ -13,6 +18,18 @@ fun ProfileScreen(navController: NavHostController) {
                 onDeleteAllConfirmed = {}
             )
         },
-        content = {}
+        content = {
+            ProfileContent(
+                apiResponse = RequestState.Success(ApiResponse(success = true)),
+                messageBarState = MessageBarState(),
+                firstName = "",
+                lastName = "",
+                onFirsNameChanged = {},
+                onLastNameChanged = {},
+                emailAddress = "",
+                profilePhoto = "",
+                onSignOutClicked = {}
+            )
+        }
     )
 }
